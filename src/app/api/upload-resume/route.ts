336286de3864e70deefc93225df6b5ace267b689
RaihanWebDev <raihantariq919@gmail.com>
2026-07-09 19:24:02 +0500

    Fix resume upload for Vercel with Cloudinary

--- src/app/api/upload-resume/route.ts
+++ src/app/api/upload-resume/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import pdf from "pdf-parse-new";
 import cloudinary from "@/lib/cloudinary";
+
+import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf.mjs";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/legacy/build/pdf.worker.mjs",
+  import.meta.url
+).toString();
 
 export async function POST(req: NextRequest) {
   try {
@@ -14,7 +20,9 @@ export async function POST(req: NextRequest) {
           success: false,
           message: "Resume is required",
         },
-        { status: 400 }
+        {
+          status: 400,
+        }
       );
     }
 
@@ -24,15 +32,15 @@ export async function POST(req: NextRequest) {
           success: false,
           message: "Only PDF files are allowed",
         },
-        { status: 400 }
+        {
+          status: 400,
+        }
       );
     }
 
     const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
 
-    // Extract resume text
-    const data = await pdf(buffer);
+    const buffer = Buffer.from(bytes);
 
     // Upload PDF to Cloudinary
     const uploadResult = await new Promise<any>((resolve, reject) => {
@@ -42,18 +50,37 @@ export async function POST(req: NextRequest) {
           resource_type: "raw",
         },
         (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
+          if (error) reject(error);
+          else resolve(result);
         }
       );
 
       stream.end(buffer);
     });
 
+    // Extract PDF text
+    const pdf = await pdfjsLib.getDocument({
+      data: new Uint8Array(buffer),
+    }).promise;
+
+    let resumeText = "";
+
+    for (let page = 1; page <= pdf.numPages; page++) {
+      const currentPage = await pdf.getPage(page);
+
+      const content = await currentPage.getTextContent();
+
+      const pageText = content.items
+        .map((item: any) => ("str" in item ? item.str : ""))
+        .join(" ");
+
+      resumeText += pageText + "\n";
+    }
+
     return NextResponse.json({
       success: true,
       resumeUrl: uploadResult.secure_url,
-      resumeText: data.text,
+      resumeText,
     });
   } catch (error) {
     console.error(error);
